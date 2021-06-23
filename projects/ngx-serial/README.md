4c8ccf2883c8184bb711eb64ccdcd25e1645eec4
Alejandro Rioja <archocron@gmail.com>
2021-06-22 22:00:22 -0500

    version 1.0.1

--- projects/ngx-serial/README.md
+++ projects/ngx-serial/README.md
@@ -6,54 +6,82 @@ You can establish a bidirectional communication with any device supporting RS232
 
 Examples of devices could be Arduino like boards, PLC's, Smart Card Readers, Access control devices.
 
+This [Video](https://www.youtube.com/watch?v=Hsl_imkJa7o) show the library in action 
+
 ## Installation
 
 First, install the package using npm install
 
-    $ npm install ngx-serial
+    npm install ngx-serial
 
 ## Use
 
 You have available ngx-serial on your project, add the following import in your component.
 
-    $ import { NgxSerial } from 'ngx-serial';
+    import { NgxSerial } from 'ngx-serial';
 
 You can use the class as follow.
 
-    $ serial: NgxSerial;
+    serial: NgxSerial;
 
-    $ constructor() {
-    $   this.serial = new NgxSerial(this.dataHandler);
-    $ }
+    constructor() {
+        this.serial = new NgxSerial(this.dataHandler);
+    }
 
-Data Handler will receive read data, so here you can implement your read logic. It will read until CR + LF is received.
+Data Handler will receive read data, so here you can implement your read logic. It will read until LF (can be changed passing the string to the constructor) is received.
 
-    $ dataHandler(data: string) {
-    $   console.log(data);
-    $ }
+    dataHandler(data: string) {
+        console.log(data);
+    }
 
 To request permissions to use the COM port and connect , use the method connect(). 
 
-    $ this.serial.connect(); 
+    this.serial.connect(); 
 
 To use connect() a user interaction is required, for example call this method when the user click a button.
 
 To send data to the serial device use sendData(), this method accept a string as paremeter.
 
-    $ this.serial.sendData("Data\r\n");
+    this.serial.sendData("Data\n");
 
 To free resources execute the method close()
 
-    $ this.serial.close();
+    this.serial.close();
 
 Default options values are.
 
-    $ baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none'
+    baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none'
 
-In the constructor you can pass optionally an options Object.
+In the constructor you can pass optionally an options Object and the control character (\n by default).
 
-    $ let options = { baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none' };
-    $ this.serial = new NgxSerial(this.dataHandler, options);
+    let options = { baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none' };
+    this.serial = new NgxSerial(this.dataHandler, options, "\n");
+
+connect() and close() accept a callback function returning the serial port. Can be used to check the connection state.
+
+    if(!this.port){
+        this.serial.connect((port:any)=>{
+        this.port = port;
+    });
+
+    if(this.port)
+        this.serial.sendData("L1\n");
+
+    if(this.port)
+        this.serial.close((port:any)=>{
+        this.port = port;
+    });
+
+
+     <div *ngIf="this.port">
+        <span class = "greendot"></span>
+        <span>Connected</span>
+    </div>
+    <div *ngIf="!this.port">
+        <span class = "reddot"></span>
+        <span>Disconnected</span>
+    </div>
+
 
  To check an example application made on Angular click [here](https://github.com/archocron/ngx-serial-example) 
  The Arduino Code is located on src/assets/ngx-serial-example.ino
--- projects/ngx-serial/src/lib/ngx-serial.ts
+++ projects/ngx-serial/src/lib/ngx-serial.ts
@@ -1,125 +1,120 @@
 import { NONE_TYPE } from "@angular/compiler";
 
 export class NgxSerial {
-    
-  /*autoconnect: boolean = false;
-  index: number = -1;*/
-  private port: any;
-  private options = { baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none' }; //Default
-  private writer: any;
-  private  readFunction:Function;
 
-  /*constructor(readFunction:Function, autoconnect?: boolean, index?: number) {
-    this.readFunction = readFunction;
-    if (autoconnect) {
-        this.autoconnect = autoconnect;
+    private port: any;
+    private options = { baudRate: 9600, dataBits: 8, parity: 'none', bufferSize: 256, flowControl: 'none' }; //Default
+    private writer: any;
+    private readFunction: Function;
+    private controlCharacter: string = "\n";
+    private reader:any;
+    private readableStreamClosed:any;
+    private writableStreamClosed:any;
+    private keepReading:boolean = true;
+  
+    constructor(readFunction: Function, options?: any, controlCharacter?: any) {
+      this.readFunction = readFunction;
+      if (options)
+        this.options = options;
+      if (controlCharacter)
+        this.controlCharacter = controlCharacter;
+  
     }
-
-    if (index) {
-        this.index = index;
+    public async sendData(data: string) {
+      await this.writer.write(data);
     }
-    
-}*/
-constructor(readFunction:Function, options?:any) {
-    this.readFunction = readFunction;
-    if(options)
-       this.options = options;
-       
-}
- public  async sendData(data: string) {
-    await this.writer.write(data);
-}
-
-
- 
- 
-  private async readLoop() {  
-      while (this.port.readable) {
+  
+    private async readLoop() {
+      
+  
+      while (this.port.readable && this.keepReading ) {
         const textDecoder = new TextDecoderStream();
-      const readableStreamClosed = this.port.readable.pipeTo(textDecoder.writable);
-      const reader = textDecoder.readable
-      .pipeThrough(new TransformStream(new LineBreakTransformer()))
-      .getReader();
-          try {
-              while (true) {
-                  const { value, done } = await reader.read();
-                  if (done) {
-                      // Allow the serial port to be closed later.
-                      reader.releaseLock();
-                      break;
-                  }
-                  if (value) {
-                      //console.log(value);
-                      this.readFunction(value);
-                  }
-              }
-          } catch (error) {
-              // TODO: Handle non-fatal read error.
+        this.readableStreamClosed = this.port.readable.pipeTo(textDecoder.writable);
+        this.reader = textDecoder.readable
+          .pipeThrough(new TransformStream(new LineBreakTransformer(this.controlCharacter)))
+          .getReader();
+  
+        try {
+          while (true) {
+            const { value, done } = await this.reader.read();
+            if (done) {
+              break;
+            }
+            if (value) {
+              this.readFunction(value);
+            }
           }
+        } catch (error) {
+          console.error("Read Loop error. Have the serial device been disconnected ? ");
+        }
       }
-  }
-  public async close(){
-    
-      this.writer.releaseLock();
+    }
+    public async close(callback:Function) {
+      this.keepReading = false;
+      this.reader.cancel();
+      await this.readableStreamClosed.catch(() => {});
+      this.writer.close();
+      await this.writableStreamClosed;
       await this.port.close();
-  }
+      callback(null);
+    }
   
-  public async connect() {
+    public async connect(callback:Function) {
+      this.keepReading = true;
       if ("serial" in navigator) {
-          // The Web Serial API is supported by the browser.
-          let nav: any = navigator;
-          const ports = await nav.serial.getPorts();
+        // The Web Serial API is supported by the browser.
+        let nav: any = navigator;
+        const ports = await nav.serial.getPorts();
   
-         /* if (ports.length == 0 || !this.autoconnect) {
-              this.port = await nav.serial.requestPort();
-          } else {
-              if (this.index != -1)
-                  this.port = ports[this.index];
-              else
-                  this.port = ports[0];
-  
-          }*/
-  
-
+        try {
           this.port = await nav.serial.requestPort();
-
   
-          await this.port.open(this.options);
+        } catch (error) {
+          console.error("Requesting port error: " + error);
+          return;
+        }
   
-          const textEncoder = new TextEncoderStream();
-          const writableStreamClosed = textEncoder.readable.pipeTo(this.port.writable);    
-          this.writer = textEncoder.writable.getWriter();
-  
-          this.readLoop();
+        try {
+         await this.port.open(this.options);
   
   
+        } catch (error) {
+          console.error("Opening port error: " + error);
+          return;
+        }
+  
+        const textEncoder = new TextEncoderStream();
+        this.writableStreamClosed = textEncoder.readable.pipeTo(this.port.writable);
+        this.writer = textEncoder.writable.getWriter();
+  
+        this.readLoop();
+  
+        callback(this.port);
   
       } else {
-          console.error("This browser does NOT support serial API");
-  
+        console.error("This browser does NOT support the Web Serial API");
       }
   
+    }
   }
-}
-
-class LineBreakTransformer {
-    chunks:any = "";
-    constructor() {
-      // A container for holding stream data until a new line.
-      this.chunks = "";
+  
+  class LineBreakTransformer {
+    container: any="";
+    private controlCharacter: string;
+  
+    constructor(controlCharacter: string) {
+      this.container = '';
+      this.controlCharacter = controlCharacter
     }
   
     transform(chunk:any, controller:any) {
-      // Append new chunks to existing chunks.
-      this.chunks += chunk;
-      // For each line breaks in chunks, send the parsed lines out.
-      const lines = this.chunks.split("\r\n");
-      this.chunks = lines.pop();
+      this.container += chunk;
+      const lines = this.container.split(this.controlCharacter);
+      this.container = lines.pop();
       lines.forEach((line: any) => controller.enqueue(line));
     }
   
     flush(controller:any) {
-      // When the stream is closed, flush any remaining chunks out.
-      controller.enqueue(this.chunks);
+      controller.enqueue(this.container);
     }
   }
